7dfb7d676acd3f0cc7e917cbe60c002a56e5ce4b
Mohammed Hamioui <r0880734@student.thomasmore.be>
2022-12-16 00:50:46 +0100

    Add project files.

---
+++ Games.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GIDOCS
+{
+    public class Games
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Developer { get; set; }
+
+        public string Publisher { get; set; }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GIDOCS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Games> DatabaseGames { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Create() {
+            using (Datacontext context= new Datacontext()) {
+                var Title = TitleTextBox.Text;
+                var Genre = GenreTextBox.Text;
+                var Developer = DeveloperTextBox.Text;
+                var Publisher = PublisherTextBox.Text;
+
+                if ((Title != null) && (Genre != null) && (Developer != null) && (Publisher != null)) {
+                    context.VideoGames.Add(new Games() { Title = Title, Genre = Genre, Developer = Developer, Publisher = Publisher });
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Read()
+        {
+            using (Datacontext context = new Datacontext())
+            {
+                DatabaseGames = context.VideoGames.ToList();
+                ItemList.ItemsSource = DatabaseGames;
+            }
+        }
+
+        public void Update()
+        {
+            using (Datacontext context = new Datacontext())
+            {
+                Games selectedGame = ItemList.SelectedItem as Games;
+                
+                var Title = TitleTextBox.Text;
+                var Genre = GenreTextBox.Text;
+                var Developer = DeveloperTextBox.Text;
+                var Publisher = PublisherTextBox.Text;
+
+                if ((Title != null) && (Genre != null) && (Developer != null) && (Publisher != null))
+                {
+                    Games game = context.VideoGames.Find(selectedGame.Id);
+                    game.Title = Title;
+                    game.Genre = Genre;
+                    game.Developer = Developer;
+                    game.Publisher = Publisher;
+
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete()
+        {
+            using (Datacontext context = new Datacontext()) {
+                 Games selectedGame = ItemList.SelectedItem as Games;
+
+                if (selectedGame != null)
+                {
+                    Games game = context.VideoGames.Single(x=> x.Id == selectedGame.Id);
+
+                    context.Remove(game);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Create();
+        }
+
+        private void ReadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Read();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Delete();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ItemList.Items.Clear();
+        }
+    }
+}
